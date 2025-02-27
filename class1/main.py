e8cf1d04e4d55f0c25ede1b02ef6c7b538897f3b
Maham-Ahsan <160377217+Maham-Ahsan@users.noreply.github.com>
2025-02-27 17:39:45 +0500

    first commit

---
+++ class1/main.py
@@ -0,0 +1,39 @@
+# Python DataTypes:
+
+# Integer: A whole number without a fractional component.
+
+integer_number = 10
+
+# Float: A number that has a fractional component.
+
+float_number = 3.14
+
+# Boolean: A data type that can only have two values: True or False.
+
+boolean_value = True
+
+# String: A sequence of characters enclosed in either single quotes (' ') or double quotes (" ").
+
+string_value = "Hello, World!"
+
+# List: An ordered collection of elements. Lists are mutable (they can be changed).
+
+list_items = ["apple", "banana", "cherry", [-4, 5]]
+
+# Tuple: An ordered collection of elements. Tuples are immutable (they cannot be changed).
+
+tuple_items = ("apple", "banana", "cherry", "Mango")
+
+# Set: An unordered collection of unique elements. Sets are mutable (they can be changed).
+
+set_items = {"apple", "banana", "cherry"}
+
+# Dictionary: A collection of key-value pairs. Dictionaries are mutable (they can be changed).
+
+dictionary_items = {"name": "John", "age": 30, "city": "New York"}
+
+
+# Printing the values:
+
+print(integer_number, float_number, boolean_value , string_value, list_items, tuple_items, set_items, dictionary_items)
+
